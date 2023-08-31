39394d39e28f4f0569a64b92a1cc3c2413a789df
Redwarh <132839683+Redwarh@users.noreply.github.com>
2023-08-31 04:49:35 -0600

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-const app_e = express();
-app_e.listen('5000',function(){
-    console.log('aplicación iniciada en : http://localhost:5000/')
-})
-app_e.get('/',function(req,res){
-    res.send("Hola mundo")
-})
